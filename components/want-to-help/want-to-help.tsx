99e03ee2f3399bd25e9eec630ac11fb465bca063
arturbunko <artur.bunko@gen.tech>
2022-06-10 10:39:03 +0300

    feat: change font size for want to help module

--- components/want-to-help/want-to-help.tsx
+++ components/want-to-help/want-to-help.tsx
@@ -5,10 +5,10 @@ export const WantToHelp = () => {
 
   return (
     <section id="#want-to-help" className="lg:mt-36 mt-14">
-      <h2 className="font-kharkiv xl:text-h1 lg:text-h2 text-h0Mob mix-blend-overlay tracking-tighter xl:mb-10 lg:mb-10">
+      <h2 className="font-kharkiv xl:text-[110px] lg:text-h2 text-h0Mob mix-blend-overlay tracking-tighter xl:mb-10 lg:mb-10">
         {t('Want to help')}
       </h2>
-      <a className="relative font-kharkiv xl:text-h1 lg:text-h2 text-h0Mob bg-gradient-to-r from-blue to-yellow bg-clip-text text-fill-transparent cursor-pointer hover:before:absolute hover:before:bottom-0 hover:before:left-[15%] hover:before:border-2 hover:before:border-arrow hover:before:w-full">
+      <a className="relative font-kharkiv xl:text-[110px] lg:text-h2 text-h0Mob bg-gradient-to-r from-blue to-yellow bg-clip-text text-fill-transparent cursor-pointer hover:before:absolute hover:before:bottom-0 hover:before:left-[15%] hover:before:border-2 hover:before:border-arrow hover:before:w-full">
         {t('Donate Now')}
       </a>
     </section>
