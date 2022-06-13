33ffc3cc1474ad9772591193f60a76f9ab04d4d9
arturbunko <artur.bunko@gen.tech>
2022-06-13 12:52:26 +0300

    feat: dd

--- components/want-to-help/want-to-help.tsx
+++ components/want-to-help/want-to-help.tsx
@@ -1,14 +1,29 @@
 import { useTranslations } from 'use-intl';
+import { useRouter } from 'next/router';
 
 export const WantToHelp = () => {
+  const router = useRouter();
   const t = useTranslations('Want to help');
+
+  const isUkrainian = router.locale === 'ua';
 
   return (
     <section id="#want-to-help" className="lg:mt-36 mt-14">
-      <h2 className="font-kharkiv xl:text-[110px] lg:text-h2 text-[56px] mix-blend-overlay tracking-tighter xl:mb-10 lg:mb-10">
+      <h2
+        className={[
+          'font-kharkiv lg:text-h2 text-[56px] mix-blend-overlay tracking-tighter xl:mb-10 lg:mb-10',
+          isUkrainian ? 'xl:text-[110px]' : 'xl:text-h0',
+        ].join(' ')}
+      >
         {t('Want to help')}
       </h2>
-      <a className="relative font-kharkiv xl:text-[110px] lg:text-h2 text-[56px] bg-gradient-to-r from-blue to-yellow bg-clip-text text-fill-transparent cursor-pointer hover:before:absolute hover:before:bottom-0 hover:before:left-[15%] hover:before:border-2 hover:before:border-arrow hover:before:w-full">
+      <a
+        className={[
+          'relative font-kharkiv lg:text-h2 text-[56px] bg-gradient-to-r from-blue to-yellow bg-clip-text text-fill-transparent cursor-pointer',
+          'hover:before:absolute hover:before:bottom-0 hover:before:left-[15%] hover:before:border-2 hover:before:border-arrow hover:before:w-full',
+          isUkrainian ? 'xl:text-[110px]' : 'xl:text-h0',
+        ].join(' ')}
+      >
         {t('Donate Now')}
       </a>
     </section>
