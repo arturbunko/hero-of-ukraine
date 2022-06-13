5a8d3e487b9e8414b1ccdce21329ce43a40422a5
arturbunko <artur.bunko@gen.tech>
2022-06-13 12:48:29 +0300

    fix: translations

--- components/heading/heading.tsx
+++ components/heading/heading.tsx
@@ -2,18 +2,27 @@ import dynamic from 'next/dynamic';
 
 import { Button } from '../button/button';
 import { useTranslations } from 'use-intl';
+import { useRouter } from 'next/router';
 
 const Map = dynamic(() => import('../map/map'), { ssr: false });
 
 export const Heading = () => {
+  const router = useRouter();
   const t = useTranslations('Heading');
+
+  const isUkrainian = router.locale === 'ua';
 
   return (
     <section id="heading" className="flex flex-col lg:mb-44 mb-8">
       <div className="self-end xl:w-[1050px] lg:w-[950px] lg:h-[702px] w-[434px] h-[290px] z-10">
         <Map className="self-end" />
       </div>
-      <div className="flex flex-col lg:-mt-[26rem] mt-[-18.5rem]">
+      <div
+        className={[
+          'flex flex-col lg:-mt-[26rem] mt-[-18.5rem]',
+          isUkrainian ? 'lg:-mt-[28rem]' : 'lg:-mt-[26rem]',
+        ].join(' ')}
+      >
         <h1 className="tracking-tighter xl:text-h1 lg:text-h2 text-h2Mob xl:leading-none lg:leading-none leading-none text-white font-kharkiv w-full lg:max-w-full lg:max-w-[895px] max-w-[390px]">
           {t('title')}
         </h1>
