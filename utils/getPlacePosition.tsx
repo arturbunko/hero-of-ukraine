71722c3fe8d29c65f75c7ac70435a45029b4b1d5
arturbunko <artur.bunko@gen.tech>
2022-08-02 10:21:49 +0300

    fix: place inde

--- utils/getPlacePosition.tsx
+++ utils/getPlacePosition.tsx
@@ -5,14 +5,12 @@ export const getPlacePosition = (initial: string, current: string): number => {
   const initialIndex = Places.findIndex((value) => value.path === initial);
   const currentIndex = Places.findIndex((value) => value.path === current);
 
-  console.log({ placesCount, initialIndex, currentIndex });
-
   const isCurrent = initialIndex === currentIndex;
 
   if (isCurrent) return 1;
 
   if (currentIndex > initialIndex) {
-    return placesCount - (placesCount - currentIndex + 2);
+    return currentIndex - initialIndex + 1;
   }
 
   return Places.length - Math.abs(initialIndex - currentIndex) + 1;
