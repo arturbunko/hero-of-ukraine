1f5bb80adb75ea651237389ce410537ab397d71b
arturbunko <artur.bunko@gen.tech>
2022-08-04 12:40:49 +0300

    fix

--- components/donate-modal/error-message.tsx
+++ components/donate-modal/error-message.tsx
@@ -18,7 +18,7 @@ export const ErrorMessage = ({ onClose }: { onClose: () => void }) => {
     <div
       id="modal"
       ref={errorRef}
-      className="relative w-full h-full flex flex-col items-center justify-center lg:text-h2 text-h2Mob font-kharkiv"
+      className="relative w-full h-full flex flex-col items-center justify-center lg:text-h2 text-h2Mob leading-normal font-kharkiv"
     >
       <button className="absolute top-10 left-10" onClick={onClose}>
         <svg
