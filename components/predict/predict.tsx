f2571c43aeaa17fc809b2d723639849e0c5e4b6f
arturbunko <artur.bunko@gen.tech>
2022-08-15 11:48:12 +0300

    feat: more space in predict section

--- components/predict/predict.tsx
+++ components/predict/predict.tsx
@@ -18,7 +18,7 @@ export const Predict = () => {
           loading="lazy"
         />
       </div>
-      <div className="flex md:flex-col flex-row lg:w-full justify-between md:w-[40%] w-full md:mt-0 mt-3 lg:ml-[10%] md:ml-[5%] ml-0 md:gap-0 gap-3">
+      <div className="flex md:flex-col flex-row lg:w-full justify-between md:w-[40%] w-full md:mt-0 mt-6 lg:ml-[10%] md:ml-[5%] ml-0 md:gap-0 gap-3">
         <ImagePath
           className="lg:w-[532px] md:w-full w-1/2 lg:h-[400px] lg:object-center  h-[218px] object-cover"
           src="/Depositphotos_567758610_XL 1.jpg"
@@ -28,7 +28,7 @@ export const Predict = () => {
           height={400}
           loading="lazy"
         />
-        <p className="lg:text-body text-bodyMob text-white md:self-end self-start lg:w-[472px] lg:mt-0 md:mt-2 mt-0 w-full">
+        <p className="lg:text-body text-[15px] text-white md:self-end self-start lg:w-[472px] lg:mt-0 md:mt-2 mt-0 w-full">
           {t('text')}
         </p>
       </div>
